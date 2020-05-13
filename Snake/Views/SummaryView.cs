2758d5007620b40c7590f02c891bf0e4a5771e65
Holovin <denys.holovin@cgi.com>
2020-05-13 19:59:38 -0300

    Removed ShouldPlayAgain() from SummaryView as it belongs to Game itself.

--- Snake/Views/SummaryView.cs
+++ Snake/Views/SummaryView.cs
@@ -41,31 +41,6 @@ namespace Snake.Views
             await Task.CompletedTask;
         }
 
-        /* Key Codes:
-            n/N     110/78
-            y/Y     121/89
-            CTRL+C  3
-            ESC     27
-            ENTER   13
-        */
-        public async Task<bool> ShouldPlayAgain(CancellationToken cancellationToken)
-        {
-            byte? key = null;
-            await _inputOutputService.Print(StartX + 1, StartY + Height - 1, Constants.SHOULD_PLAY_AGAIN, cancellationToken);
-
-            // TODO: should Key Codes be a part of IInputOutputService, though, not every implementation might have KeyCodes?
-            while (key != 27 && key != 110 && key != 78 && key != 121 && key != 89 && key != 3) // 'ESC'|n|N|y|Y|CTRL+C
-            {
-                if (key != null) {
-                    // await _inputOutputService.Print(String.Format("Unsupported key '{0}'", key.ToString()), cancellationToken);
-                }
-
-                key = await _inputOutputService.GetKey(cancellationToken);
-            }
-
-            return await Task.FromResult(key == 89 || key == 121); // 'true' if 'Y'/'y'
-        }
-
         public async Task DrawBorder(CancellationToken cancellationToken) {
             string topHorizontBlock = await _figureService.GetTopHorizontBlock(cancellationToken);          // "▄";
             string bottomHorizontBlock = await _figureService.GetBottomHorizontBlock(cancellationToken);    // "▀";
