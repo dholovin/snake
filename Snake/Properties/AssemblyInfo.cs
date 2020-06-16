0094cd060c48c7ac11a815181da718157bb88b1b
Holovin <denys.holovin@cgi.com>
2020-06-16 18:46:01 -0300

    Change approach to generate Assumbly Info

---
+++ Snake/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("Terminal Snake")]
+[assembly: AssemblyCompany("Home Furnace")]
+[assembly: AssemblyCopyright("Copyright Denys Holovin ©2020")]
+
+[assembly: Guid("4e4f2d33-aaab-48ea-a63d-1f0a8e3c935f")]
+[assembly: ComVisible(false)] //not going to expose ;)
+
+// Version information for an assembly consists of the following four values:
+// roughly translated from I reckon it is for SO, note that they most likely 
+// dynamically generate this file
+//      Major Version  - Year 6 being 2016
+//      Minor Version  - The month
+//      Day Number     - Day of month
+//      Revision       - Build number
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below: [assembly: AssemblyVersion("year.month.day.*")]
+[assembly: AssemblyVersion("1.0.0.1")] 
+[assembly: AssemblyFileVersion("1.0.0.1")]
